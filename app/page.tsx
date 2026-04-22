fc8fab75aeb2e5e89473159aae9095fe2aa4771b
diyaadinesh <diyadinesh0t7@gmail.com>
2026-04-22 13:16:08 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 My First DevOps Project</h1>
+      <p>This is my Next.js static website</p>
+    </main>
+  );
+}
